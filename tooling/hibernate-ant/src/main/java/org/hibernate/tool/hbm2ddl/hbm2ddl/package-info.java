c06e41e285dc2f044fc86748f1b1fa5688b9de00
Koen Aers <koen.aers@gmail.com>
2025-10-21 14:07:11 +0200

    Merge Hibernate Tools into ORM : Remove the accidentally duplicated 'hbm2ddl' folder in the 'Ant' module

--- tooling/hibernate-ant/src/main/java/org/hibernate/tool/hbm2ddl/hbm2ddl/package-info.java
+++
@@ -1,9 +0,0 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- * Copyright Red Hat Inc. and Hibernate Authors
- */
-
-/**
- * The {@code hbm2ddl} tool.
- */
-package org.hibernate.tool.hbm2ddl.hbm2ddl;
